05c2df520a7d8949b97ea42b9e8a21878fb1a455
Joao Uva <joaouva@gmail.com>
2021-02-08 12:21:16 +0000

    Updated with dark mode

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import './App.css';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Project from './components/Projects';
 import Footer from './components/Footer';
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavigationBar from './components/NavigationBar';
-import { DarkToggle } from './components/DarkToggle';
+import { Sun, Moon } from "react-feather";
 
 
+function App() {
+  
+  
+  const storedDarkMode = localStorage.getItem("DARK_MODE");
+  const [darkMode, setDarkMode] = useState(storedDarkMode);
+  const toggleDarkMode = () => setDarkMode(!darkMode);
 
-class App extends React.Component {
+  useEffect(() => {
+		console.log(`Is in dark mode? ${darkMode}`);
+		localStorage.setItem("DARK_MODE", darkMode);
+  }, [darkMode]);
 
-  render() {
+
+  
    return (
-     <div className="App">
-       <DarkToggle/>
-       <NavigationBar />
-			 <Header />
-       <AboutMe />
+		<div className="App" data-theme={darkMode ? "dark" : "light"}>
+			<button onClick={toggleDarkMode} className="theme-switch-button">
+				{darkMode ? <Sun /> : <Sun />}
+			</button>
+			<NavigationBar />
+			<Header />
+			<AboutMe />
 			<Project />
 			<Footer />
 		</div>
-  );
-  }
+   );
 }
 
 export default App;
