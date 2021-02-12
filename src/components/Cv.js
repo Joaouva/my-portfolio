474cc0f37c718e4cd304ca4671a6171c465ad085
Joao Uva <joaouva@gmail.com>
2021-02-12 12:09:43 +0000

    google analytics

---
+++ src/components/Cv.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+class CV extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <div className="cv-header">
+                    <h1></h1>
+                    <img></img>
+                </div>
+                <div>
+                    <h1></h1>
+                    <div>
+                        <div>
+                            <h2></h2>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CV
